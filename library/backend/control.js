c796d2fda808b4b5b5a3e9866f2dd4d4d13aed30
mikofranc <m.ogbechie@native.semicolon.africa>
2023-08-31 19:26:53 +0100

    Just added control.js

---
+++ library/backend/control.js
@@ -0,0 +1,10 @@
+const firstname =document.querySelector("#firstname").value;
+const lastname =document.querySelector("#lastname").value;
+const phone =document.querySelector("#phonenumber").value;
+const email =document.querySelector("#email").value;
+const pass =document.querySelector("#password").value;
+const verify_pass = document.querySelector("#verify_password").value;
+
+
+const login_email =document.querySelector("#login_email").value;
+const login_pass = document.querySelector("#login_password").value;
